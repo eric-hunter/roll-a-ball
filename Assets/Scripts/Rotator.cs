e16ae1e39eb5260d0b7d56137e60dbdc1294ed29
ej <ejames788@gmail.com>
2019-03-08 20:27:31 -0500

    Added pickup item.

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //by multiplying the v3 by deltaTime, we get smooth animation on frame
+        //updates.
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
